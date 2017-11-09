210e86cef91007d7c1379824111422b802d737ef
David Kironde <david.kironde@andela.com>
2017-11-09 21:51:10 +0300

    clean up code using class based components

--- memorygrid/src/App.js
+++ memorygrid/src/App.js
@@ -2,32 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Cell extends Component{
+  render(){
+    return(
+      <div className = "cell" style = {{ width:'20%' }}></div>
+    )
+  }
+};
+
 class Game extends Component {
     render(){
         return(
             <div className = "Game">
                <div className="grid">
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-               <div className="cell" style={{ width: '20%' }}></div>
-             </div>
-                    <button>
-                      Start Game.
-                      </button>
-                      <div className = "message">Game message here... </div>
+                <Cell/><Cell/><Cell/><Cell/> 
+                <Cell/><Cell/><Cell/><Cell/> 
+                <Cell/><Cell/><Cell/><Cell/> 
+                <Cell/><Cell/><Cell/><Cell/> 
+                </div>
+                <button>
+                Start Game.
+                </button>
+                <div className = "message">Game message here... </div>
             </div>
         );
     }
