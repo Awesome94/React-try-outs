a34875922e66cd0852a0289d912519201481e53b
David Kironde <david.kironde@andela.com>
2017-11-28 22:56:38 +0300

    add css configurations and make code sexy

--- numbersgame/src/App.js
+++ numbersgame/src/App.js
@@ -32,17 +32,36 @@ const Stars = (props) => {
   </div>
   );
   }
+  // const Numbers = (props) => {
+  //   return(
+  //   <div>
+  //   <span>1</span>
+  //   <span>2</span>
+  //   <span>3</span>
+  //   <span>4</span>
+  //   <span>5</span>
+  //   </div>
+  //   )
+  // }
+// The numbers above where generated manually. Below we generate them automatically.
   const Numbers = (props) => {
-    return(
-    <div>
-    <span>1</span>
-    <span>2</span>
-    <span>3</span>
-    <span>4</span>
-    <span>5</span>
-    </div>
-    )
-  }
+    const numberClassName = (number) => {
+    if (props.selectedNumber.indexOf(number) >= 0) {
+    return 'selected';
+    }
+    }
+      return(
+      <div className = "card text-center">
+      <div>
+      {Numbers.list.map((number, i) =>
+       <span key = {i}>{number}</span>
+      )}
+     </div>
+      </div>
+      );
+    };
+    Numbers.list = _.range(1, 10);
+    
   
   class Game extends React.Component{
   render(){
@@ -50,8 +69,8 @@ const Stars = (props) => {
   <div layout="row">
   <Stars/>
   <Button/>
-  <Answer/>
-  <Numbers/>
+  <Answer selectedNumber = {this.selectedNumber}/>
+  <Numbers selectedNumber = {this.selectedNumber}/>
   </div>
   );
   }
