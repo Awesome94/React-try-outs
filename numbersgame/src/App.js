a12db2cd22b412ba32d6fdef067af787816428bf
David Kironde <david.kironde@andela.com>
2017-11-30 18:07:22 +0300

    add possible combination function

--- numbersgame/src/App.js
+++ numbersgame/src/App.js
@@ -65,7 +65,54 @@ const Stars = (props) => {
     Numbers.list = _.range(1, 10);
     
   
-    
+    class Game extends React.Component{
+      state = {
+      selectedNumbers: [],
+      randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+      usedNumbers: [],
+      answerIsCorrect: null,
+      };
+      
+      selectNumber = (clickedNumber) => {
+      if (this.state.selectedNumbers.indexOf(clickedNumber) >=0) {return;}
+      this.setState(prevState => ({
+      answerIsCorrect: null,
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+      }));
+      };
+      
+      unselectNumber = (clickedNumber) => {
+      this.setState(prevState => ({
+      answerIsCorrect: null,
+      selectedNumbers: prevState.selectedNumbers
+      .filter(number => number !== clickedNumber)
+      }));
+      };
+      
+      checkAnswer = () => {
+      this.setState(prevSate => ({
+      answerIsCorrect: prevSate.randomNumberOfStars === prevSate.selectedNumbers.reduce((acc, n) => acc+n, 0)
+        }));
+      };
+      
+      render(){
+      //const { selectedNumbers, randomNumberOfStars } = this.state;
+      return(
+      <div className="container">
+      <h3>Play Nine</h3>
+      <div className="row">
+      <Stars numberOfStars={this.state.randomNumberOfStars}/>
+      <Button selectedNumbers={this.state.selectedNumbers}
+              checkAnswer={this.checkAnswer}
+              answerIsCorrect={this.state.answerIsCorrect}/>
+      <Answer selectedNumbers={this.state.selectedNumbers} 
+      unselectNumber = {this.unselectNumber}/>
+      </div>
+      <Numbers selectedNumbers={this.state.selectedNumbers} selectNumber={this.selectNumber}/>
+      </div>
+      );
+      }
+      }
   
   class App extends React.Component {
   render(){
