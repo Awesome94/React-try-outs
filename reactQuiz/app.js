819aea21f8f6a9eb8117e51d0a93cd37695e754e
David Kironde <david.kironde@andela.com>
2018-01-27 09:13:59 +0300

    add jsx and event handlers

--- reactQuiz/app.js
+++ reactQuiz/app.js
@@ -1,18 +1,65 @@
+import { fail } from "assert";
+
 /**@jsx React.Dom */
 (function (){
     'use strict';
     var quiz = React.creatClass({
-        render: function(){
-            return <div>test</div>;
-        }
-    });
+        propTypes:{
+            data: React.propTypes.array.isRequired
+        },
+        getInitialState: function(){
+            return{
+                this.props.data.selectGame();
+                // author: this.props.data[0],
+                // books: this.props.data[0].books
+            };
+        },
+        handleBookSelected: function(title){
+            var isCorrect = this.state.checkAnswer(title);
+            this.setState({
+                bgClass: isCorrect ? 'pass' : 'fail',
+                showContinue: isCorrect
+            });
+        },
+        
+        handleContinue: function(){
+            this.setState(this.getInitialState());
+        },
+        
+        render: function(){ 
+            return (<div>
+                <div className="row">
+                <div className="col-md-4">
+                <img src={this.state.author.imgUrl} className="author"
+                </div>
+                <div className="col-md-7">
+                {this.state.books.map(function(b){
+                    return <Book onBookSelected={this.handleBookSelected} title={b}/>;
+                }, this)}
+                </div>
+                <div className={"col-md-1" + this.state.bgClass}/>
+                </div>
+                {this.state.showContinue?(
+                        <div className="row">
+                        <div className="col-md-12">
+                        <input onClick={this.handleContinue} type="button" className="btn btn"
+                        </div> 
+                        </div>):<span/>
+                           }
+                        </div>
+                    );
+                }
+            });
 
     var Book = React.CreateClass({
         proptypes: {
             title: React.propTypes.String.isRequired
         },
+        handleClick: function(){
+            this.props.onBookSelected(this.props.tilte)
+        }
         render: function (){
-            return <div><h4>{this.props.tilte}</h4></div>;
+            return <div onClick={this.handleClick} className="answer"><h4>{this.props.tilte}</h4></div>;
         
         }
     });
@@ -51,7 +98,30 @@
             books: ["Romeo and Juliet"]
         }
 
-    ]
+    ];
+
+    data.selectGame = function(){
+        var books = _.shuffle(this.reduce(function(p,c,i){
+            return p.concat(c.books);
+        },[])).slice(0,4);
+
+    var answer = books[_.random(books.length-1)];
+    return {
+        books: books,
+        author: _.find(this, function(author){
+            return author.books.some(function(title){
+                return title === answer;
+            });
+        })
+    };
+};
+data.checkAnswer = function(title){
+    return this.author.books.some(function(t){
+        return t === tilte;
+
+        
+    })
+}
     
     React.render()
 })();
