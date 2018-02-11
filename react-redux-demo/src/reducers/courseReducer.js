df9e74a1d1f89afeb173ee2dc0814e0690209e21
David Kironde <david.kironde@andela.com>
2018-02-11 10:52:10 +0300

    add map to state and dispatch

--- react-redux-demo/src/reducers/courseReducer.js
+++ react-redux-demo/src/reducers/courseReducer.js
@@ -1,6 +1,7 @@
+import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action){
-    swith(action.type){
-        case 'CREATE_COURSE':
+    switch(action.type) {
+        case types.CREATE_COURSE:
         return [...state, 
         Object.assign({}, action.course)
     ];
